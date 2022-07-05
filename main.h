a42a4994fd3bf65b37c019447c92e87ffcf739f0
Salvador Borit <5084@holbertonschool.com>
2022-07-05 14:02:36 -0700

    created files

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+
+#endif // MAIN_H
---
+++ printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _printf - produces output according to format
+* @format: string that holds what will be printed
+* Return: string that specifies how subsequent arguments
+* (or arguments accessed via the variable-length argument
+* facilities of stdarg(3)) are converted for output
+*/
+int _printf(const char *format, ...)
+{
+	
+}
